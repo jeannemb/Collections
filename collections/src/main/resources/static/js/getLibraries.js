a4998f4b2a3bd49c4d34fdc2a30964d71a4e36fe
wlodarmt <wlodarmt@gmail.com>
2017-11-12 12:21:59 -0600

    Get Items from libraries
    Get Items from libraries - javascript finished?  still needs cleaning up to look pretty

--- collections/src/main/resources/static/js/getLibraries.js
+++ collections/src/main/resources/static/js/getLibraries.js
@@ -13,17 +13,47 @@ $( document ).ready(function() {
 			url : "http://localhost:8080/manage/library?userId=1",
 			success: function(result){
 					$('#getResultDiv ul').empty();
-					var library = result[0].name;
 					result.forEach(function(element){
-						library += element.name;
+						var library = library + " " + element.name;
+						var t = document.createTextNode(element.name);
+					    var btn = document.createElement("BUTTON");
+					    btn.appendChild(t);
+					    btn.setAttribute("id", element.libraryId);
+					    document.getElementById("libraryGroup").appendChild(btn);
+						$("#" + element.libraryId).click(function(event){
+							event.preventDefault();
+							ajaxGetItems(element.libraryId, element.type);
+						});
 					});
 					console.log("Success: ", result[0].name);
-					$("#getResultDiv").html(library);
 			},
 			error : function(e){
 				$("#getResultDiv").html("<strong>Error2</strong>");
 				console.log("ERROR: ", e);
 			}
 		});
+	}
+	
+	// GET Items
+	function ajaxGetItems(libId, libType){
+		var URL = "http://localhost:8080/manage/items?libraryId=" + libId + "&libraryType=" + libType;
+		$.ajax({
+			type : "GET",
+			url : URL,
+			success: function(result){
+				$('#getResultDiv ul').empty();
+				var itemList = "";
+				result.forEach(function(element){
+					itemList += element.title + "\n";
+				});
+				console.log("Success: ", URL);
+				$("#getResultDiv").html(itemList);
+			},
+			error : function(e){
+				$("#getResultDiv").html("<strong>Error3</strong>");
+				console.log("ERROR: Couldn't retrieve Items", URL);
+			}
+		});
+
 	}
 })
