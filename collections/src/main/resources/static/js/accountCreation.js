f41c22a63588276291f95bb1a2e4815f2a5a2694
Meharbi88 <32585759+Meharbi88@users.noreply.github.com>
2018-01-10 20:57:52 -0600

    implement accountCreation in the front end using Angular and javascript

--- collections/src/main/resources/static/js/accountCreation.js
+++ collections/src/main/resources/static/js/accountCreation.js
@@ -1,4 +1,34 @@
-var app = angular.module('app', []);
-app.controller('controller', function($scope, $filter) {
+var app = angular.module('collection', []);
+app.controller('accountCreation', function($scope, $http) {
+		
+	$scope.create = function(){
+		var user = {
+			firstName : $scope.firstName,
+			lastName : $scope.lastName,
+			email : $scope.email,
+		}
+			
+
+		$http({
+			method: "POST",
+			url: "http://localhost:8080//user/createUser",
+			data : angular.toJson(user),
+			headers : {
+					'Content-Type': 'application/json'
+			}
+		}).then(_success, _error);
+		
+		
+		function _success(response){
+			$scope.response = response.data
+		}
+		
+		function _error(response){
+			$scope.response = response.statusText
+
+		}
+
+	};
+	
 	
 });
