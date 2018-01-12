cd3c142a06a9fe9b41a06c7adc0567dee5e9891b
Meharbi88 <32585759+Meharbi88@users.noreply.github.com>
2018-01-12 14:30:09 -0600

    adding if condition to check the passweord and confirm password are match

--- collections/src/main/resources/static/js/accountCreation.js
+++ collections/src/main/resources/static/js/accountCreation.js
@@ -2,32 +2,38 @@ var app = angular.module('collection', []);
 app.controller('accountCreation', function($scope, $http) {
 		
 	$scope.create = function(){
-		var user = {
-			firstName : $scope.firstName,
-			lastName : $scope.lastName,
-			email : $scope.email,
-		}
+		if($scope.password == $scope.confirmPassword){
+			
+		
+			var user = {
+					firstName : $scope.firstName,
+					lastName : $scope.lastName,
+					email : $scope.email,
+			}
 			
 
-		$http({
-			method: "POST",
-			url: "http://localhost:8080//user/createUser",
-			data : angular.toJson(user),
-			headers : {
+			$http({
+				method: "POST",
+				url: "http://localhost:8080//user/createUser",
+				data : angular.toJson(user),
+				headers : {
 					'Content-Type': 'application/json'
+				}
+			}).then(_success, _error);
+		
+		
+			function _success(response){
+				$scope.response = response.data
 			}
-		}).then(_success, _error);
 		
-		
-		function _success(response){
-			$scope.response = response.data
-		}
-		
-		function _error(response){
-			$scope.response = response.statusText
+			function _error(response){
+				$scope.response = response.statusText
 
-		}
+			}
 
+		}else{
+			$scope.response = "Password must match confirm Password"
+		}
 	};
 	
 	
