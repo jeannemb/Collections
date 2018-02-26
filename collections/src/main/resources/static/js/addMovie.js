57b17cdeda0d1896064220a327f02dfead749f46
Meharbi88 <32585759+Meharbi88@users.noreply.github.com>
2018-02-25 18:37:21 -0600

    Adding actors to movie and TV show search.

--- collections/src/main/resources/static/js/addMovie.js
+++ collections/src/main/resources/static/js/addMovie.js
@@ -9,6 +9,7 @@ app.controller('addMovie', function($scope,$http) {
 	$scope.responseResult = false;
 	$scope.showSearchBar = true;
 	$scope.showManualEntry = false;
+	var myMovies = [];
 	//$scope.LibraryName = localStorage.getItem("libraryId");
 	//$scope.LibraryName = $scope.LibraryName + " " + localStorage.getItem("type");
 	
@@ -37,7 +38,7 @@ app.controller('addMovie', function($scope,$http) {
 	
 	performMovieSerach = function(url){
 	
-		var myMovies = [];
+		myMovies = [];
 		var searchText = encodeURI($scope.searchText);
 		url = url + searchText;
 		var settings = {
@@ -49,50 +50,70 @@ app.controller('addMovie', function($scope,$http) {
 				  "data": "{}"
 		}
 		
-		$.ajax(settings).done(function (response) {
-				  console.log(response);
-				  if(typeof response.results !== 'undefined' || response.total_results != 0){
-					  $scope.nothingFound = false;
-					  $scope.searchResult = true;
-					  for(i=0; i<response.results.length;i++){
-						  var movie = new Object();
-						  if (typeof response.results[i].original_title !== 'undefined'){
-								movie.title = response.results[i].original_title;
-						  }
-						  
-						  if (typeof response.results[i].poster_path !== 'undefined'){
-								movie.posterUrl = "https://image.tmdb.org/t/p/w500"+response.results[i].poster_path;	
-							}else{
-								movie.posterUrl = "/images/placeholderImageItem.png";
-							}
-						  
-						  movie.owns = false;
-						  movie.wantsToOwn = false;
-						  movie.complete = false;
-						  movie.wantsToComplete = false;
-						  myMovies.push(movie); 
-					  }
-					  $scope.result = true;
-					  $scope.movies = myMovies;
-					  $scope.showSearchBar = true;
-					  $scope.showManualEntry = false;
-					  $scope.response = "LookUp Clicked";
-					  $scope.alertSuccess = false;
-					  $scope.alertDanger = false;
-					  $scope.$apply();
-				  }else{
-						$scope.movies = myMovies;
-						$scope.nothingFound = true;
-						$scope.searchResult = false;
-						$scope.$apply();
+		$http({
+			method : "GET",
+			url: url,
+			data : settings
+		}).then(function successCallback(response) {
+			response.data.results.forEach(function(element){
+				//console.log(element)
+				var movie = new Object();
+				movie.id = element.id;
+				if (typeof element.original_title !== 'undefined'){
+					movie.title = element.original_title;
 				}
+						  
+				if (typeof element.poster_path !== 'undefined'){
+					movie.posterUrl = "https://image.tmdb.org/t/p/w500"+element.poster_path;	
+				}else{
+					movie.posterUrl = "/images/placeholderImageItem.png";
+				}
+				movie.actors = "";
+				var url = "https://api.themoviedb.org/3/movie/"+movie.id+"/credits?api_key=188771a883b3cc1762e81ae2666c52fc";
+				var settings = {
+						  "async": true,
+						  "crossDomain": true,
+						  "url": url,
+						  "method": "GET",
+						  "headers": {},
+						  "data": "{}"
+						}
+				$http({
+					method : "GET",
+					url: url,
+					data : settings
+				}).then(function successCallback(response) {
+					for (j=0; j<response.data.cast.length; j++){
+						//console.log(element)
+						movie.actors = movie.actors + response.data.cast[j].name;
+						if(j==5){
+							break;
+						}
+						movie.actors = movie.actors + ", ";
+					}
+					
+				}, function errorCallback(response) {
+					
+				});
+				//console.log(movie.actors);
+				movie.owns = false;
+				movie.wantsToOwn = false;
+				movie.complete = false;
+				movie.wantsToComplete = false;
+				myMovies.push(movie);
+				
+			});
+			showResult();
+		}, function errorCallback(response) {
+			showError();
 		});
+
 	}
-	
+
 	
 	performTvShowSerach = function(url){
 		
-		var myMovies = [];
+		myMovies = [];
 		var searchText = encodeURI($scope.searchText);
 		url = url + searchText;
 		var settings = {
@@ -104,52 +125,84 @@ app.controller('addMovie', function($scope,$http) {
 				  "data": "{}"
 		}
 		
-		$.ajax(settings).done(function (response) {
-				  console.log(response);
-				  if(typeof response.results !== 'undefined' || response.total_results != 0){
-					  $scope.nothingFound = false;
-					  $scope.searchResult = true;
-					  for(i=0; i<response.results.length;i++){
-						  var movie = new Object();
-						  if (typeof response.results[i].original_name !== 'undefined'){
-								movie.title = response.results[i].original_name;
-						  }
-						  
-						  if (typeof response.results[i].poster_path !== 'undefined'){
-								movie.posterUrl = "https://image.tmdb.org/t/p/w500"+response.results[i].poster_path;	
-							}else{
-								movie.posterUrl = "/images/placeholderImageItem.png";
-							}
-						  movie.owns = false;
-						  movie.wantsToOwn = false;
-						  movie.complete = false;
-						  movie.wantsToComplete = false;
-						  myMovies.push(movie); 
-					  }
-					  $scope.result = true;
-					  $scope.movies = myMovies;
-					  $scope.showSearchBar = true;
-					  $scope.showManualEntry = false;
-					  $scope.response = "LookUp Clicked";
-					  $scope.alertSuccess = false;
-					  $scope.alertDanger = false;
-					  $scope.$apply();
-				  }else{
-						$scope.movies = myMovies;
-						$scope.nothingFound = true;
-						$scope.searchResult = false;
-						$scope.$apply();
+		$http({
+			method : "GET",
+			url: url,
+			data : settings
+		}).then(function successCallback(response) {
+			response.data.results.forEach(function(element){
+				//console.log(element)
+				var movie = new Object();
+				movie.id = element.id;
+				if (typeof element.original_name !== 'undefined'){
+						movie.title = element.original_name;
 				}
+						  
+				if (typeof element.poster_path !== 'undefined'){
+					movie.posterUrl = "https://image.tmdb.org/t/p/w500"+element.poster_path;	
+				}else{
+					movie.posterUrl = "/images/placeholderImageItem.png";
+				}
+				movie.actors = "";
+				var url = "https://api.themoviedb.org/3/tv/"+movie.id+"/credits?api_key=188771a883b3cc1762e81ae2666c52fc";
+				var settings = {
+						  "async": true,
+						  "crossDomain": true,
+						  "url": url,
+						  "method": "GET",
+						  "headers": {},
+						  "data": "{}"
+						}
+				$http({
+					method : "GET",
+					url: url,
+					data : settings
+				}).then(function successCallback(response) {
+					for (j=0; j<response.data.cast.length; j++){
+						//console.log(element)
+						movie.actors = movie.actors + response.data.cast[j].name;
+						if(j==5){
+							break;
+						}
+						movie.actors = movie.actors + ", ";
+					}
+				}, function errorCallback(response) {
+					
+				});
+				//console.log(movie.actors);
+				movie.owns = false;
+				movie.wantsToOwn = false;
+				movie.complete = false;
+				movie.wantsToComplete = false;
+				myMovies.push(movie);
+				
+			});
+			showResult();
+		}, function errorCallback(response) {
+			showError();
 		});
+		  
 	}
 	
-	
-	
-	
-	getDetails = function(id){
-		
+	function showResult(){
+		$scope.nothingFound = false;
+		$scope.searchResult = true;
+		  $scope.result = true;
+		  $scope.movies = myMovies;
+		  $scope.showSearchBar = true;
+		  $scope.showManualEntry = false;
+		  $scope.response = "LookUp Clicked";
+		  $scope.alertSuccess = false;
+		  $scope.alertDanger = false;
+		  $scope.$apply();
 	}
 	
+	function showError(){
+		$scope.movies = myMovies;
+		$scope.nothingFound = true;
+		$scope.searchResult = false;
+		$scope.$apply();
+	}
 	
 	$scope.addMovie = function(movie){
 		console.log(movie);
